de898dc7c3c40d8b94b6f0ec371335afa1a8e387
Jørn Støylen <jorn@prikkprikkprikk.no>
2019-06-06 20:08:44 +0200

    Add Carbon Fields

--- functions.php
+++ functions.php
@@ -9,6 +9,24 @@
  * @package wp_rig
  */
 
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
+function crb_attach_theme_options() {
+    Container::make( 'theme_options', __( 'Theme Options' ) )
+        ->add_fields( array(
+            Field::make( 'text', 'crb_text', 'Text Field' ),
+        ) );
+}
+
+add_action( 'after_setup_theme', 'crb_load' );
+function crb_load() {
+    require_once( 'vendor/autoload.php' );
+    \Carbon_Fields\Carbon_Fields::boot();
+}
+
+
 define( 'WP_RIG_MINIMUM_WP_VERSION', '4.5' );
 define( 'WP_RIG_MINIMUM_PHP_VERSION', '7.0' );
 
